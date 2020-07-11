b9775cb38d115a15c47634178b47fe2ce6b22f3d
JanBloch <jan.bloch@noseryoung.ch>
2020-07-11 19:00:06 +0200

    Added Exception InvalidSyntaxException

---
+++ InvalidSyntaxException.java
@@ -0,0 +1,8 @@
+public class InvalidSyntaxException extends Exception{
+    public InvalidSyntaxException(){
+        super();
+    }
+    public InvalidSyntaxException(String text){
+        super(text);
+    }
+}
